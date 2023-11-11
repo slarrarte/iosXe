f869919e68f0155f28e7136121098710162769c7
slarrarte <slarrarte@gmail.com>
2023-11-11 14:30:43 -0500

    Second commit

--- xeTemplate.py
+++ xeTemplate.py
@@ -3,116 +3,116 @@ from pathlib import Path
 import getRunningConfig, json, loadConfiguration, getCapabilities, customRESTCONF, saveConfig
 from pathlib import Path
 
-# # Change hostname
-# newHostname = """
-# {
-#   "Cisco-IOS-XE-native:hostname": "sancorp-r1"
-# }
-# """
-#
-# print(
-#     customRESTCONF.customRESTCONF(
-#         'patch',
-#         '10.10.20.48',
-#         'Cisco-IOS-XE-native:native/hostname',
-#         'developer',
-#         'C1sco12345',
-#         strData=newHostname
-#     )
-# )
-#
-# # Change banner
-# newBanner = """
-# {
-#   "Cisco-IOS-XE-native:login": {
-#       "banner": "Banner updated via RESTCONF"
-#   }
-# }
-# """
-#
-# print(
-#     customRESTCONF.customRESTCONF(
-#         'patch',
-#         '10.10.20.48',
-#         'Cisco-IOS-XE-native:native/banner/login',
-#         'developer',
-#         'C1sco12345',
-#         strData=newBanner
-#     )
-# )
-#
-# # Create local account
-# newLocalAccount = """
-# {
-#   "Cisco-IOS-XE-native:username": [
-#     {
-#       "name": "sysAdmin",
-#       "privilege": 1,
-#       "secret": {
-#         "encryption": "9",
-#         "secret": "$9$2uJqUsbtLlISaE$2BvT6b.W0phNXnUxNPzjppa19H0N1JbTT0PDXcWZfdM"
-#       }
-#     }
-#   ]
-# }
-# """
-#
-# print(
-#     customRESTCONF.customRESTCONF(
-#         'patch',
-#         '10.10.20.48',
-#         'Cisco-IOS-XE-native:native/username',
-#         'developer',
-#         'C1sco12345',
-#         strData=newLocalAccount
-#     )
-# )
-#
+# Change hostname
+newHostname = """
+{
+  "Cisco-IOS-XE-native:hostname": "sancorp-r1"
+}
+"""
+
+print(
+    customRESTCONF.customRESTCONF(
+        'patch',
+        '10.10.20.48',
+        'Cisco-IOS-XE-native:native/hostname',
+        'developer',
+        'C1sco12345',
+        strData=newHostname
+    )
+)
+
+# Change banner
+newBanner = """
+{
+  "Cisco-IOS-XE-native:login": {
+      "banner": "Banner updated via RESTCONF"
+  }
+}
+"""
+
+print(
+    customRESTCONF.customRESTCONF(
+        'patch',
+        '10.10.20.48',
+        'Cisco-IOS-XE-native:native/banner/login',
+        'developer',
+        'C1sco12345',
+        strData=newBanner
+    )
+)
+
+# Create local account
+newLocalAccount = """
+{
+  "Cisco-IOS-XE-native:username": [
+    {
+      "name": "sysAdmin",
+      "privilege": 1,
+      "secret": {
+        "encryption": "9",
+        "secret": "$9$2uJqUsbtLlISaE$2BvT6b.W0phNXnUxNPzjppa19H0N1JbTT0PDXcWZfdM"
+      }
+    }
+  ]
+}
+"""
+
+print(
+    customRESTCONF.customRESTCONF(
+        'patch',
+        '10.10.20.48',
+        'Cisco-IOS-XE-native:native/username',
+        'developer',
+        'C1sco12345',
+        strData=newLocalAccount
+    )
+)
+
 # Create keychain
-# keyChain = """
-# {
-#   "Cisco-IOS-XE-crypto:chain": [
-#     {
-#       "name": "EIGRP69",
-#       "key": [
-#         {
-#           "id": "69",
-#           "cryptographic-algorithm-choice": {
-#             "default": "hmac-sha-256"
-#           },
-#           "cryptographic-algorithm": "hmac-sha-256",
-#           "key-string": {
-#             "encryption": "7",
-#             "key": "1423373838"
-#           },
-#           "accept-lifetime": {
-#             "lifetime-group-v1": {
-#               "start-hh-mm-ss": "00:00:00",
-#               "start-month": "Oct",
-#               "start-day": 31,
-#               "start-year": 2023,
-#               "infinite": [null]
-#             }
-#           }
-#         }
-#       ]
-#     }
-#   ]
-# }
-# """
-#
-# print(
-#     customRESTCONF.customRESTCONF(
-#         'patch',
-#         '10.10.20.48',
-#         'Cisco-IOS-XE-native:native/key/Cisco-IOS-XE-crypto:chain',
-#         'developer',
-#         'C1sco12345',
-#         strData=keyChain
-#     )
-# )
-#
-# # Create routing domain
+keyChain = """
+{
+  "Cisco-IOS-XE-crypto:chain": [
+    {
+      "name": "EIGRP69",
+      "key": [
+        {
+          "id": "69",
+          "cryptographic-algorithm-choice": {
+            "default": "hmac-sha-256"
+          },
+          "cryptographic-algorithm": "hmac-sha-256",
+          "key-string": {
+            "encryption": "7",
+            "key": "1423373838"
+          },
+          "accept-lifetime": {
+            "lifetime-group-v1": {
+              "start-hh-mm-ss": "00:00:00",
+              "start-month": "Oct",
+              "start-day": 31,
+              "start-year": 2023,
+              "infinite": [null]
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
+"""
+
+print(
+    customRESTCONF.customRESTCONF(
+        'patch',
+        '10.10.20.48',
+        'Cisco-IOS-XE-native:native/key/Cisco-IOS-XE-crypto:chain',
+        'developer',
+        'C1sco12345',
+        strData=keyChain
+    )
+)
+
+# Create routing domain
 eigrpInstance = """
 {
   "Cisco-IOS-XE-eigrp:router-eigrp": {
@@ -140,7 +140,7 @@ eigrpInstance = """
 
 print(
     customRESTCONF.customRESTCONF(
-        'get',
+        'patch',
         '10.10.20.48',
         'Cisco-IOS-XE-native:native/router/Cisco-IOS-XE-eigrp:router-eigrp',
         'developer',
