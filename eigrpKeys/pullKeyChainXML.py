cc82e55745b13506c70b8d232dc03d83e3d9d163
slarrarte <100368309+slarrarte@users.noreply.github.com>
2024-10-01 21:26:50 -0400

    Update pullKeyChainXML.py

--- eigrpKeys/pullKeyChainXML.py
+++ eigrpKeys/pullKeyChainXML.py
@@ -5,13 +5,13 @@ from pathlib import Path
 xml_filter = open(Path.home()/'pyProjects/projects/iosXeLab/eigrpKeys/keyChain.xml').read()
 
 # List all hosts to update
-host_ips = ['172.16.100.12', '172.16.100.13', '172.16.100.14']
+host_ips = ['']
 
 text_for_document = """"""
 
 # Obtain device key chain xml data for verification, then put output in a file
 for i in host_ips:
-    test_file = open(Path.home()/f'pyProjects/projects/iosXeLab/eigrpKeys/test{i}.xml', 'w')
+    test_file = open(Path.home()/f'', 'w')
     test_file.write(
         netconfActions.netconfGetConfig(
             i,
@@ -23,5 +23,3 @@ for i in host_ips:
         )
     )
     test_file.close()
-
-# xml_file = open(Path.home()/'pyProjects/projects/iosXeLab/eigrpKeys/test.xml').read()
