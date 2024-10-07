8e334714fd885b9cbdafd6b8c5f2bdb2198164f2
slarrarte <100368309+slarrarte@users.noreply.github.com>
2024-10-07 15:53:27 -0400

    Update eigrpKeyChainPull.py

--- eigrpKeys/eigrpKeyChainPull.py
+++ eigrpKeys/eigrpKeyChainPull.py
@@ -1,7 +1,7 @@
 import xmltodict, netconfActions
 from pathlib import Path
 
-# Key chain NETCONF filter
+# Path to key chain NETCONF filter
 xml_filter = """
 <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
     <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
@@ -37,17 +37,18 @@ for i in host_ips:
     key_chain_path = xml_to_dict['rpc-reply']['data']['native']['key']['chain']['key']
     # Aformentioned for loop that generates text for the key chain report
     for j in range(num_of_keys):
-        text_for_document += f"\tKey {key_chain_path[j]['id']}\n"
-        text_for_document += f"\t\tKey String: {key_chain_path[j]['key-string']['key']}\n"
-        text_for_document += f"\t\tSend Lifetime:\n\t\t\t"
-        text_for_document += f"Start Time: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-hh-mm-ss']}\n\t\t\t"
-        text_for_document += f"Start Date: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-year']}\n\t\t\t"
-        text_for_document += f"End Time: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-hh-mm-ss']}\n\t\t\t"
-        text_for_document += f"End Date: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-year']}\n"
-        text_for_document += f"\t\tAccept Lifetime: \n\t\t\t"
-        text_for_document += f"Start Time: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['start-hh-mm-ss']}\n\t\t\t"
-        text_for_document += f"Start Date: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['start-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-year']}\n\t\t\t"
-        text_for_document += f"End Time: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['end-hh-mm-ss']}\n\t\t\t"
-        text_for_document += f"End Date: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['end-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-year']}\n"
-with open('', 'w') as file:
+        text_for_document += f'''Key {key_chain_path[j]['id']}
+    Key String: {key_chain_path[j]['key-string']['key']}
+    Send Lifetime:
+        Start Time: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-hh-mm-ss']}
+        Start Date: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-year']}
+        End Time: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-hh-mm-ss']}
+        End Date: {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-year']}
+    Accept Lifetime:
+        Start Time: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['start-hh-mm-ss']}
+        Start Date: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['start-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['start-year']}
+        End Time: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['end-hh-mm-ss']}
+        End Date: {key_chain_path[j]['accept-lifetime']['lifetime-group-v1']['end-month']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-day']} {key_chain_path[j]['send-lifetime']['lifetime-group-v1']['end-year']}\n\n'''
+
+with open(Path.home()/'', 'w') as file:
     file.write(text_for_document)
